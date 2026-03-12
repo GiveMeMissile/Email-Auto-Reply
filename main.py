a48ed30c443a87fcfe9bb23f5645118c0e5e4824
Dylan <givememissiles@gmail.com>
2026-03-11 21:57:06 -0500

    Using my School Portal to access gmail for now, Will eventually change it to allow for direct access

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+from time import sleep
+import json
+
+def browser(playwright):
+    # Set Headless to False for testing and if you want to see the browser at work, keep it True otherwise
+    browser = playwright.chromium.launch(channel="msedge", headless=False)
+
+    # Create context for web browser
+    context = browser.new_context()
+
+    # Open new tab and go to gmail
+    tab1 = context.new_page()
+    tab1.goto("https://aisd-tx.us001-rapididentity.com/p/portal")
+    sleep(1)
+
+    # Note: I will be accessing gmail through my school's portal until I am able to directly access gmail 
+    # through a google account, password setup for use will be set up later.
+    # Log into school gmail
+    with open("info.json", "r") as f:
+        data = json.load(f)
+    tab1.get_by_label("Username").fill(data["Username"])
+    tab1.get_by_role("button", name="Go").click()
+    sleep(1)
+    tab1.get_by_role("textbox", name="password").fill(data["Password"])
+    tab1.get_by_role("button", name="Go").click()
+    sleep(2)
+
+    with context.expect_page() as new_tab:
+        tab1.get_by_role("button", name="Gmail").first.click()
+    sleep(10)
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        browser(playwright)
