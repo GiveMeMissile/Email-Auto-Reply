13772e42c572dbd0fbff104ae7b50997fac92397
Dylan <givememissiles@gmail.com>
2026-03-16 20:36:39 -0500

    Added LLM responces to the email, it is not a very good responce, and it needs help ending the message as well as handling the context, though it shall soon be done!

--- llm_handler.py
+++ llm_handler.py
@@ -1,1 +1,54 @@
-# Will be used to generate responces, how I will do that has yet to be determined, so for now this will be just an empty file ):
+from transformers import AutoTokenizer, AutoModelForImageTextToText, BitsAndBytesConfig
+import torch
+
+class LLMManager:
+
+    LLM_IN_USE = "Qwen/Qwen3.5-4B-Base"
+    context = "context.txt"
+
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.LLM_IN_USE)
+        config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+        self.model = AutoModelForImageTextToText.from_pretrained(
+            self.LLM_IN_USE,
+            quantization_config=config,
+            device_map="auto"
+        )
+        with open (self.context, "r") as f:
+            self.template = f.read().replace("\n", "", 3)
+
+    def prepare_input(self, email_text, sender):
+        ai_input = self.template
+        ai_input = ai_input.replace("<sender>", sender)
+        ai_input = ai_input.replace("<email>", email_text)
+        return ai_input
+
+    def forward(self, email_text, sender, ai_text): 
+
+        # Prepare AI input
+        with torch.no_grad():
+            ai_input = self.prepare_input(email_text, sender)
+            ai_input = ai_input + ai_text
+            ai_input = self.tokenizer(ai_input, return_tensors="pt").to(self.device)
+            
+            # Get singular text token from AI model
+            output = self.model(ai_input["input_ids"], attention_mask=None)
+            output = output.logits[:, -1, :]
+            output = torch.argmax(output, dim=-1).type(torch.int64)
+            generated_text = self.tokenizer.decode(output.item())
+
+            # Clear Tensors from GPU memory
+            del ai_input
+            del output
+            torch.cuda.empty_cache()
+
+            # Return single token text output (duh)
+            return generated_text
+    
+    def stop_generating(self, token_text):
+
+        if token_text == self.tokenizer.decode(self.tokenizer.eos_token_id):
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from playwright.sync_api import sync_playwright
-from time import sleep # I really want to do this, yet I musn't...
+from time import sleep
+from llm_handler import LLMManager
 import email_processor
 import json
 
-def browser(playwright):
+
+# Constants IG
+PASSWORD_FILE = "info.json"
+# Using my school website to access gmail bc google does not like scrapers ):
+START_WEBSITE = "https://aisd-tx.us001-rapididentity.com/p/portal"
+MAX_TOKENS = 256
+
+
+def browser(playwright, ai_manager):
     # Set Headless to False for testing and if you want to see the browser at work, keep it True otherwise
     browser = playwright.chromium.launch(channel="msedge", headless=False)
 
@@ -18,7 +27,7 @@ def browser(playwright):
     # Note: I will be accessing gmail through my school's portal until I am able to directly access gmail 
     # through a google account, password setup for use will be set up later.
     # Log into school gmail
-    with open("info.json", "r") as f:
+    with open(PASSWORD_FILE, "r") as f:
         data = json.load(f)
     tab1.get_by_label("Username").fill(data["Username"])
     tab1.get_by_role("button", name="Go").click()
@@ -38,9 +47,9 @@ def browser(playwright):
 
     # Get the names of the unread Emails
     unread_emails = email_processor.get_unread_emails(gmail_html)
-    sleep(2)
-    access_email(tab2, unread_emails[0])
-    sleep(5)
+    email_text = access_email(tab2, unread_emails[0])
+    sleep(1)
+    respond_to_email(tab2, email_text, unread_emails[0], ai_manager)
 
 def access_email(tab, info):
 
@@ -49,10 +58,33 @@ def access_email(tab, info):
 
     tab.wait_for_selector(".ii.gt")
     email_html = tab.locator(".ii.gt").inner_html()
-    email_processor.proccess_email_html(email_html)
-    
+    text = email_processor.proccess_email_html(email_html)
+    tab.wait_for_load_state("domcontentloaded")
+    tab.locator(".pYTkkf-JX-I.pYTkkf-JX-I-ql-ay5-ays.DILLkc").click()
+    tab.wait_for_load_state("domcontentloaded")
+    return text
+
+def respond_to_email(tab, email_text, info, ai_manager):
+    message_box = tab.get_by_role("textbox", name="Message Body")
+    tokens = 0
+    ai_text = ""
+    while True:
+        ai_token = ai_manager.forward(email_text, info[0], ai_text)
+        tokens += 1
+        print(f"AI Text: {ai_text}")
+        if ai_manager.stop_generating(ai_token):
+            break
+        elif tokens >= MAX_TOKENS:
+            break
+        else:
+            ai_text += ai_token
+            message_box.fill(ai_text)
+
 
 
 if __name__ == "__main__":
+    print("Setting up Local LLM...")
+    ai_manager = LLMManager()
+    print("Local LLM has been set up!")
     with sync_playwright() as playwright:
-        browser(playwright)
+        browser(playwright, ai_manager)
